36b990887a6fd0f36102bac23446b3f3b5570338
Manju6121 <abishatitini@gmail.com>
2026-03-23 15:23:09 +0530

    updated files

--- Attendance.Application/Mapping/MappingProfile.cs
+++ Attendance.Application/Mapping/MappingProfile.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Attendance.Application.Mapping
+﻿using AttendanceTracker.Application.DTOs;
+using AttendanceTracker.Application.DTOs.Role;
+using AttendenceTracker.Domain.Entity;
+using AutoMapper;
+using AttendanceTracker.Application.DTOs;
+namespace AttendanceTracker.Application.Mapping
 {
-    internal class MappingProfile
+    public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<Role, RoleDTO>().ReverseMap();
+            CreateMap<UserDetails, UserDetailsDTO>().ReverseMap();
+
+            CreateMap<User, UserResponseDTO>()
+                .ForMember(dest => dest.RoleName,
+                    opt => opt.MapFrom(src => src.Role != null ? src.Role.RoleName : ""))
+                .ForMember(dest => dest.UserDetails,
+                    opt => opt.MapFrom(src => src.UserDetails));
+
+            CreateMap<User, LoginResponseDTO>().ReverseMap();
+        }
     }
 }
